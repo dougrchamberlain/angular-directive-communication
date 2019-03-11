4e4744391c8840393918c12327dfc85cb6268906
Doug Chamberlain <dougrchamberlain@gmail.com>
2019-03-10 21:15:27 -0400

    full refactor karma test framework added.

---
+++ src/app/core/footer/footer.spec.js
@@ -0,0 +1,5 @@
+describe("footer", function () {
+    it("should expect a fail", function () {
+        expect(1).toBe(2);
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,63 +1,18 @@
+import 'normalize.css';
 import 'angular';
 import 'angular-animate';
 import 'angular-aria';
 import 'angular-material';
-import 'angular-ui-router';
 import 'angular-material/angular-material.css';
+import 'angular-ui-router';
 
-import chartLayout from './foo.html';
-
-console.log(chartLayout);
+import './style.css';
 
 angular.module('myApp',
-  ['ngMaterial', 'ui.router', 'ngAnimate'])
-  .controller('myController', ['$mdSidenav',
-    function ($mdSidenav) {
+  ['ngMaterial', 'ngAnimate', 'ui.router'])
+  .controller('myController', [
+    function () {
       //fake gettting data from a database. work more quickly
       var vm = this;
-
-      vm.$onInit = function () {
-        vm.allergies = require('./data/allergies');
-        vm.getEncounter();
-      };
-
-      vm.getEncounter = function () {
-        vm.encounters = require('./data/encounters');
-        vm.setEncounter(vm.encounters[0]);
-      }
-
-      vm.setEncounter = function (encounter) {
-        vm.encounter = encounter;
-        vm.updateSections();
-      }
-
-      vm.updateSections = function () {
-        vm.sections = Object.assign({}, require('./data/sections'));
-        console.log('sections', vm);
-      }
-
-      vm.toggleMenu = function () {
-        $mdSidenav('encounter-list').toggle();
-      };
-
-      vm.filterAllergies = function (search) {
-        console.log('allergies');
-        return vm.allergies.filter(function (a) { return new RegExp(search).test(a); })
-      }
     }
-  ]).config(['$urlRouterProvider', function ($urlRouterProvider) {
-    //if not route is provided, always go to /encounter
-    //$urlRouterProvider.otherwise('/encounter');
-    console.log($urlRouterProvider);
-  }]).config(['$stateProvider', function ($stateProvider) {
-    console.log($stateProvider);
-
-  }]).directive('chartLayout', [function () {
-    return {
-      transclude: {
-        'header': '?chartHeader',
-        'body': '?chartBody'
-      },
-      template: chartLayout
-    }
-  }])
+  ]);
